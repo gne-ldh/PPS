8170c5bea33206f2a225bc8797d83f69b2ebd577
hsrai <hardeep.rai@gmail.com>
2020-02-21 10:10:54 +0530

    Code without comments

---
+++ Content/Extra/cSansComments/201printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)  
+{   int distance = 100; 
+    printf("\n\nOuput of C code developed by Hardeep Singh Rai.\n\n");
+    printf("GNE is %d km away from my house.\n\n\n", distance);
+    return 0;  
+}
