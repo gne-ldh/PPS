cb8c240ac1b57b5f1326397f7cc8c04fd4520303
Hardeep <hardeep.rai@gmail.com>
2020-11-17 11:50:59 +0530

    Create rectangleFunction.c

---
+++ docs/Files/C/rectangleFunction.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int width = 20, height = 20; // More than one variables may be declared in a single statemenmt
+
+    rect(width, height);
+
+    return 0;
+}
+
+
+
+
+void rect(int col, int row)
+{   
+    for (int i=0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if ( (i == 0) || (i == row-1) || (j == 0) || (j == col-1) ) printf("#");
+            else printf(" "); 
+        }
+        printf("\n");
+    }
+    printf("\nlast line in function\n");
+}
